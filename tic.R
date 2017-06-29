0da709b4ffa3fc6124a66da488d6cc946533fecd
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-29 03:36:59 +0200

    shortcut

--- tic.R
+++ tic.R
@@ -1,14 +1,4 @@
-get_stage("before_install") %>%
-  add_step(step_run_code(update.packages(ask = FALSE)))
-
-get_stage("install") %>%
-  add_step(step_run_code(remotes::install_deps(dependencies = TRUE)))
-
-get_stage("script") %>%
-  add_step(step_rcmdcheck())
-
-get_stage("after_success") %>%
-  add_step(step_run_code(covr::codecov(quiet = FALSE)))
+add_package_checks()
 
 if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
   # pkgdown documentation can be built optionally. Other example criteria:
@@ -17,9 +7,7 @@ if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("before_deploy") %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_add_to_known_hosts("github.com")) %>%
-    add_step(step_test_ssh())
+    add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
     add_step(step_setup_push_deploy(
