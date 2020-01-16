651e744d66d1e563d2aee804a482c64e66235714
pat-s <patrick.schratz@gmail.com>
2020-01-16 22:06:15 +0100

    use macro do_drat()

--- tic.R
+++ tic.R
@@ -1,20 +1,3 @@
 do_package_checks()
 
-if (Sys.getenv("id_rsa") != "" && ci()$get_branch() == "master") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh('id_rsa'))
-
-  get_stage("deploy") %>%
-    add_step(step_setup_push_deploy(
-      path = "~/git/drat",
-      branch = "master",
-      remote = paste0("git@github.com:", gsub("/.*$", "/tic.drat.repo", ci()$get_slug()), ".git")
-    )) %>%
-    add_step(step_add_to_drat()) %>%
-    add_step(step_do_push_deploy(path = "~/git/drat"))
-}
+do_drat("ropenscilabs/tic.drat.repo")
